f1f875cd77c5d5410897c46abd6c51318946a6e8
kongeebowl <kadenp11@nycstudents.net>
2025-02-06 12:31:56 -0500

    e

--- app/src/components/ShoppingCart.vue
+++ app/src/components/ShoppingCart.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="fixed top-0 right-0 w-64 h-full bg-gray-100 p-6 border-l border-gray-300 shadow-xl">
     <h3>Shopping Cart</h3>
-    <CartCards v-for="drug in legalDrugs" :key="drug.name" :drug="drug" />
+    <CartCards v-for="drug in cart" :key="drug.name" :drug="drug" />
   </div>
 </template>
 
-<script>
+<script setup>
 import cart from './DrugCards.vue'
-import CardCards from './CartCards.vue'
+import CartCards from './CartCards.vue'
 </script>
 
 <style scoped></style>
