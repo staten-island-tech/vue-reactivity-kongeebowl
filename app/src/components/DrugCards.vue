aec32bcc1ef5aaa25d1fa4e9ca1428b484557f39
kongeebowl <kadenp11@nycstudents.net>
2025-02-12 12:32:16 -0500

    3

--- app/src/components/DrugCards.vue
+++ app/src/components/DrugCards.vue
@@ -28,7 +28,7 @@ function addCard(drug) {
   if (existingDrug) {
     existingDrug.quantity++
   } else {
-    store.cart.push({ ...drug, quantity: 1 })
+    store.cart.push({ ...drug })
   }
 }
 </script>
