8f58acfcccad665adafb277b15e53a133aa3b89d
kongeebowl1 <kadenpang09@gmail.com>
2025-02-20 15:12:13 -0500

    awe

---
+++ app/src/arrays/items.js
@@ -0,0 +1,174 @@
+export const legalDrugs = [
+  {
+    id: 1,
+    name: 'Aspirin',
+    price: 5.99,
+    function: 'Pain relief, anti-inflammatory',
+    image:
+      'https://s.france24.com/media/display/a71a2ca4-0e74-11e9-8056-005056a964fe/w:1280/p:1x1/20080307-aspirine-m_0.jpg',
+    quantity: 1,
+  },
+  {
+    id: 2,
+    name: 'Ibuprofen',
+    price: 7.49,
+    function: 'Pain relief, anti-inflammatory',
+    image: 'https://assets.fishersci.com/TFS-Assets/CCG/product-images/F309626~p.eps-650.jpg',
+    quantity: 1,
+  },
+  {
+    id: 3,
+    name: 'Ciprofloxacin',
+    price: 13.99,
+    function: 'Antibiotic, treats bacterial infections',
+    image: 'https://facesconsent.com/resources/products-images/13966/2039780798-aep021_1.jpg',
+    quantity: 1,
+  },
+  {
+    id: 4,
+    name: 'Loratadine',
+    price: 8.49,
+    function: 'Antihistamine, allergy relief',
+    image: 'https://d3ckuu7lxvlwp2.cloudfront.net/products/323164664629f7502a64a0product.webp',
+    quantity: 1,
+  },
+  {
+    id: 5,
+    name: 'Nicotine (patch)',
+    price: 10.99,
+    function: 'Smoking cessation aid',
+    image: 'https://pics.walgreens.com/prodimg/629344/450.jpg',
+    quantity: 1,
+  },
+  {
+    id: 6,
+    name: 'Diphenhydramine',
+    price: 4.99,
+    function: 'Antihistamine, sleep aid',
+    image:
+      'https://s.turbifycdn.com/aah/yhst-12533177228474/diphenhydramine-hcl-injection-50mg-ml-25x1ml-ndc-00641-0376-25-61.gif',
+    quantity: 1,
+  },
+  {
+    id: 7,
+    name: 'Omeprazole',
+    price: 9.99,
+    function: 'Acid reflux, heartburn relief',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2021/8/FF/FR/KQ/59991453/omeprazole-injection-40-mg-250x250.jpg',
+    quantity: 1,
+  },
+  {
+    id: 8,
+    name: 'Guaifenesin',
+    price: 6.49,
+    function: 'Expectorant, cough relief',
+    image: 'https://m.media-amazon.com/images/I/61ENhNrlmfL.jpg',
+    quantity: 1,
+  },
+  {
+    id: 9,
+    name: 'Hydrocodone',
+    price: 12.99,
+    function: 'Cough suppressant, pain relief',
+    image:
+      'https://s.france24.com/media/display/ebf8c7d0-83b6-11ec-ba33-005056bf30b7/w:1280/p:1x1/2022-02-01T170143Z_327640610_RC25BS9UEAQE_RTRMADP_3_USA-OPIOIDS-LITIGATION.jpg',
+    quantity: 1,
+  },
+  {
+    id: 10,
+    name: 'Ozempic',
+    price: 1139.99,
+    function: 'Makes you less big',
+    image:
+      'https://ipro.blob.core.windows.net/images/_ozempic-single-dose-1mg-insulin-1x1-20221221042022.jpg',
+    quantity: 1,
+  },
+  {
+    id: 11,
+    name: 'Fluconazole',
+    price: 15.99,
+    function: 'Antifungal, treats yeast infections',
+    image: 'https://www.pyxuspharmaceuticals.com/wp-content/uploads/2022/09/FLUCONAZOLE.png',
+    quantity: 1,
+  },
+  {
+    id: 12,
+    name: 'Dextromethorphan',
+    price: 4.29,
+    function: 'Cough suppressant',
+    image:
+      'https://s.turbifycdn.com/aah/yhst-12533177228474/dextromethorphan-polistirex-extended-release-oral-suspension-3-oz-brand-name-delsym-30mg-5ml-perrigo-45802043321-52.gif',
+    quantity: 1,
+  },
+  {
+    id: 13,
+    name: 'Ranitidine',
+    price: 7.99,
+    function: 'Antacid, reduces stomach acid',
+    image:
+      'https://image.made-in-china.com/2f0j00OBjWADhPnfGr/Ranitidine-Hydrochloride-Injection-50mg-2ml-5AMPS-box-.webp',
+    quantity: 1,
+  },
+  {
+    id: 14,
+    name: 'Loperamide',
+    price: 3.49,
+    function: 'Anti-diarrheal',
+    image: 'https://www.doctorfox.co.uk/imgs-products/sd/1x1/loperamide-capsules-2mg-capsules.jpg',
+    quantity: 1,
+  },
+  {
+    id: 15,
+    name: 'Melatonin',
+    price: 8.99,
+    function: 'Sleep aid, regulates sleep cycle',
+    image:
+      'https://cdn2.arogga.com/eyJidWNrZXQiOiJhcm9nZ2EiLCJrZXkiOiJQcm9kdWN0LXBfaW1hZ2VzXC82NDExNFwvNjQxMTQtTWVsYXRvbmluLTEwbWctMTIwLVRhYmxldHMtU2xlZXAtU3VwcGxlbWVudC1Ob24tR01PLUdsdXRlbi1GcmVlLUJ5LVBpcGluZy1Sb2NrXzRmYTZkZDRiLTY5OTYtNDY5MC1iYjI1LTYwYWZiYmU1N2FjMS01cXY0aDMud2VicCIsImVkaXRzIjpbXX0=',
+    quantity: 1,
+  },
+  {
+    id: 16,
+    name: 'Xylazine',
+    price: 0.99,
+    function: 'horse tranqs',
+    image:
+      'https://64.media.tumblr.com/f9fbc5a358de65092f914a7df93f5661/59133c10a9a7a09c-1f/s500x750/df6c5bf8f5657187bc923268ac2d7d338c3cc10e.gifv',
+    quantity: 1,
+  },
+  {
+    id: 17,
+    name: 'LEGAL Morphine',
+    price: 9.49,
+    function: 'Painkiller that makes you totally geeked! 😜👌😁🤣😎',
+    image:
+      'https://hopeharborwellness.com/wp-content/uploads/2024/07/Morphine-Withdrawal-Atlanta-GA-1536x1075.jpg',
+    quantity: 1,
+  },
+  {
+    id: 18,
+    name: 'Tren',
+    price: 3.99,
+    function: 'Makes you big 💪💪💪',
+    image: 'https://m.media-amazon.com/images/I/714-7Eu91wL._AC_UF1000,1000_QL80_.jpg',
+    quantity: 1,
+  },
+  {
+    id: 19,
+    name: 'Atorvastatin',
+    price: 18.99,
+    function: 'Cholesterol-lowering, heart disease prevention',
+    image:
+      'https://5.imimg.com/data5/SELLER/Default/2024/1/380841738/AM/VA/KC/18677272/atorvastatin-10-mg-tablets-500x500.jpg',
+    quantity: 1,
+  },
+  {
+    id: 20,
+    name: 'Levothyroxine',
+    price: 10.49,
+    function: 'Thyroid hormone replacement',
+    image:
+      'https://s.rfi.fr/media/display/474f8978-0d37-11ea-946b-005056a9aa4d/w:1280/p:1x1/000_sg1ky.jpg',
+    quantity: 1,
+  },
+]
